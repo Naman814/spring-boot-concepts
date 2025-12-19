811afdfbe20dd70c4e3a5089f40171c2c18938af
Naman814 <namanvijay814@gmail.com>
2025-12-20 03:17:49 +0530

    swagger-ui,api-docs,configure-custom-paths

--- 04-rest-crud/05-spring-boot-rest-crud-employee-with-spring-data-rest -swagger/src/main/java/com/namancodes/crud_employee/dao/EmployeeRepository.java
+++ 04-rest-crud/05-spring-boot-rest-crud-employee-with-spring-data-rest -swagger/src/main/java/com/namancodes/crud_employee/dao/EmployeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.Path;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//http://localhost:8080/swagger-ui/index.html#/ Hit this endpoint to scan all the endpoints associated with the application.
 
 //@RepositoryRestResource(path="members") By default jpa-rest take entity plural to expose endpoints, if we want to change it,we can specify using path.
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
