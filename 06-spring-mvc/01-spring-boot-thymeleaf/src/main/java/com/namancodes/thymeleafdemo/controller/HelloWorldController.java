1e1baddb7e46987b7b114adaf7d0e2bb66b99c86
Naman814 <namanvijay814@gmail.com>
2025-12-31 02:18:34 +0530

    spring-mvc reading from data - httpservletRequest

---
+++ 06-spring-mvc/01-spring-boot-thymeleaf/src/main/java/com/namancodes/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,39 @@
+package com.namancodes.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    // controller method to show initial HTML
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    // controller method to process the HTML
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result  = "Yo!" + theName;
+
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+}
