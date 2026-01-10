71926af703029a7d38d3b6bef8d02f71984086e6
Naman814 <namanvijay814@gmail.com>
2026-01-10 14:59:00 +0530

    dynamic select tag + radio button

--- 06-spring-mvc/01-spring-boot-thymeleaf/src/main/java/com/namancodes/thymeleafdemo/controller/StudentController.java
+++ 06-spring-mvc/01-spring-boot-thymeleaf/src/main/java/com/namancodes/thymeleafdemo/controller/StudentController.java
@@ -1,20 +1,32 @@
 package com.namancodes.thymeleafdemo.controller;
 
 import com.namancodes.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${app.countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
+
+
         // create a student object
         Student theStudent = new Student();
+
+
+        theModel.addAttribute("countries", countries);
 
         // add student object to the model
         theModel.addAttribute("student",theStudent);
@@ -26,7 +38,7 @@ public class StudentController {
     public String processForm(@ModelAttribute("student") Student theStudent){
 
         //log the input data
-        System.out.println("theStudent: "+theStudent.getFirstName()+" "+theStudent.getLastName());
+        System.out.println("theStudent: "+theStudent.getFirstName()+" "+theStudent.getLastName()+" "+theStudent.getCountry()+" "+theStudent.getGender());
 
         return "student-confirmation";
     }
--- 06-spring-mvc/01-spring-boot-thymeleaf/src/main/java/com/namancodes/thymeleafdemo/model/Student.java
+++ 06-spring-mvc/01-spring-boot-thymeleaf/src/main/java/com/namancodes/thymeleafdemo/model/Student.java
@@ -5,6 +5,7 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
+    private String gender;
 
     public Student(){}
 
@@ -30,5 +31,13 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
